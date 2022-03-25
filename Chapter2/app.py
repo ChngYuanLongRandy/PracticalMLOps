e831d5fa847f05de8161b1d37506713e0f84fe66
chngyuanlongrandy <chngyuanlong@gmail.com>
2022-03-25 09:02:41 +0800

    added main

--- Chapter2/app.py
+++ Chapter2/app.py
@@ -1,3 +1,4 @@
+from distutils.log import debug
 from flask import Flask
 from flask_restful import Api
 from resources.employees import EmployeeResource, EmployeesResource, ActivityEmployeeResource
@@ -8,3 +9,6 @@ api = Api(app)
 api.add_resource(EmployeesResource,'/')
 api.add_resource(EmployeeResource,'/<int:employee_id'>)
 api.add_resource(ActivityEmployeeResource,'/<int:employee_id>/active')
+
+if __name__ == 'main':
+    app.run(port=5000, debug= True)
